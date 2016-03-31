76ed69ee8251bebb1ab7a1b9a89b286b4eb6515b
Christian Cheng <christiancheng516@gmail.com>
2016-03-30 17:40:59 -0700

    Install Handlebars

---
+++ server/configure.js
@@ -0,0 +1,11 @@
+var connect = require('connect'),
+    path = require('path'),
+    // routes = require('./routes'),
+    exphbs = require('express-handlebars');
+
+module.exports = function(app) {
+    // configuration code...
+
+    return app;
+};
+
