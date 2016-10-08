379e5c8507ec5909b2a5efd96cb08ae34d9c8037
Christian Cheng <christiancheng516@gmail.com>
2016-10-08 11:38:50 -0700

    Default message

--- server.js
+++ server.js
@@ -1,54 +1,77 @@
 var express = require('express');
-var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var COURSES_COLLECTION = "courses";
+
 var app     = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res){
+// Create database variable
+var db;
 
-    console.log('test');
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-  // Scraping from UCSD's CSE course catalog  
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+
+// Scrapes UCSD's CSE course catalog
+app.get('/scrape', function(req, res){
+
+  console.log('Initializing scraping...');
+
   url = 'http://www.ucsd.edu/catalog/courses/CSE.html';
 
   request(url, function(error, response, html) {
 
       if (!error) {
         
+        // Load the webpage html
         var $ = cheerio.load(html);
-        var i = 0;
-
         var courseNum, courseName, courseUnits;
         
+        // Iterate through the courses
         $('p.course-name').each(function() {
 
-            ++i;
-            //if (i > 30) return false;
-            var course = { courseNum : "", courseName : "", courseUnits : ""};
-            data = $(this);
-            courseName = data.text();
-            course.courseName = courseName;
-            console.log(course);
+          var course = { courseNum : "", courseName : "", courseUnits : ""};
+          data = $(this);
 
-            
-            fs.writeFile('output.json', JSON.stringify(course, null, 4),
-                function(err){
+          courseName = data.text();
+          course.courseName = courseName;
+          console.log(course);
 
-              
+          db.collection(COURSES_COLLECTION).insertOne(course, function(err,
+                doc) {
 
-            })
-            
-      
-        })
-
-      }
-
-      }) ;
-})
-
-
-
-app.listen(process.env.PORT || 5000)
-console.log('Magic happens on port 5000');
-exports = module.exports = app;
-
+            if (err) {
+              handleError(res, err.message, "Failed to add course.");
+            } else {
+              res.status(201).json(doc.ops[o]);
+            }
+           });
+          
+            //fs.writeFile('output.json', JSON.stringify(course, null, 4),
+            //function(err){
+        }) // End iteration 
+      } // Endif
+  }); // End request
+});
